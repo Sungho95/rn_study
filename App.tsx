c6e8c9b8f6228a799b91c45c51ac4b14041fa044
Sungho95 <>
2024-05-16 12:40:03 +0900

    rn BottomTabs practice

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import BottomTabNavigator from './src/navigation/BottomNavigator.tsx';
+import BottomTabNavigator from './src/navigation/BottomTabNavigator.tsx';
 
 const App = () => {
   return <BottomTabNavigator />;
