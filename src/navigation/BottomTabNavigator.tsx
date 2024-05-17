022b42e153276f7c78f3d770ef2cd52c38b1f7bb
Sungho95 <tjdgh1785@gmail.com>
2024-05-17 16:55:23 +0900

    rn Header Settings

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -6,48 +6,84 @@ import CalendarScreen from '../screens/Calendar.tsx';
 import MyPageScreen from '../screens/MyPage.tsx';
 import SettingsScreen from '../screens/Settings.tsx';
 import TitleScreen from '../screens/Title.tsx';
+import {Text} from 'react-native';
+import {RFPercentage} from 'react-native-responsive-fontsize';
+import {Dimensions} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
+
+type HeaderTitleProps = {
+  title: string;
+};
+
+const windowWidth = Dimensions.get('window').width;
+const iconSize = windowWidth * 0.065;
+const marginSize = windowWidth * 0.02;
+
+const HeaderTitle: React.FC<HeaderTitleProps> = ({title}) => (
+  <Text style={{fontSize: RFPercentage(4), fontWeight: 'bold'}}>{title}</Text>
+);
+
+const LeftTitle = () => {
+  const navigation = useNavigation();
+  return (
+    <Icon
+      name="arrow-back"
+      size={iconSize}
+      style={{marginLeft: marginSize}}
+      onPress={() => navigation.navigate('Home')}
+    />
+  );
+};
 
 function BottomTabNavigator() {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Home">
         <Tab.Screen
-          name="Title"
+          name="칭호"
           component={TitleScreen}
-          options={{
-            title: '칭호',
+          options={() => ({
+            headerTitle: () => <HeaderTitle title="칭호" />,
+            headerTitleAlign: 'center',
+            headerLeft: () => <LeftTitle />,
             tabBarIcon: ({color, size}) => (
               <Icon name="emoji-events" color={color} size={size} />
             ),
-          }}
+          })}
         />
         <Tab.Screen
-          name="Calendar"
+          name="캘린더"
           component={CalendarScreen}
           options={{
-            title: '캘린더',
+            headerTitle: () => <HeaderTitle title="캘린더" />,
+            headerTitleAlign: 'center',
+            headerLeft: () => <LeftTitle />,
             tabBarIcon: ({color, size}) => (
               <Icon name="calendar-month" color={color} size={size} />
             ),
           }}
         />
         <Tab.Screen
-          name="MyPage"
+          name="내 정보"
           component={MyPageScreen}
           options={{
-            title: '내 정보',
+            headerTitle: () => <HeaderTitle title="내 정보" />,
+            headerTitleAlign: 'center',
+            headerLeft: () => <LeftTitle />,
             tabBarIcon: ({color, size}) => (
               <Icon name="person" color={color} size={size} />
             ),
           }}
         />
         <Tab.Screen
-          name="Settings"
+          name="설정"
           component={SettingsScreen}
           options={{
-            title: '설정',
+            headerTitle: () => <HeaderTitle title="설정" />,
+            headerTitleAlign: 'center',
+            headerLeft: () => <LeftTitle />,
             tabBarIcon: ({color, size}) => (
               <Icon name="settings" color={color} size={size} />
             ),
