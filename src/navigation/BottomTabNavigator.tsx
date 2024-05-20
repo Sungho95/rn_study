ae7a6012332dcfc270b15d36b7efd650268e3b6a
Sungho95 <tjdgh1785@gmail.com>
2024-05-20 14:49:02 +0900

    rn Home Screen practice

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import CalendarScreen from '../screens/Calendar.tsx';
 import MyPageScreen from '../screens/MyPage.tsx';
 import SettingsScreen from '../screens/Settings.tsx';
 import TitleScreen from '../screens/Title.tsx';
-import {Text} from 'react-native';
+import { Dimensions, Text } from 'react-native';
 import {RFPercentage} from 'react-native-responsive-fontsize';
-import {Dimensions} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from '../screens/Home.tsx';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,36 +17,40 @@ type HeaderTitleProps = {
 };
 
 const windowWidth = Dimensions.get('window').width;
-const iconSize = windowWidth * 0.065;
+const iconSize = windowWidth * 0.095;
 const marginSize = windowWidth * 0.02;
 
 const HeaderTitle: React.FC<HeaderTitleProps> = ({title}) => (
   <Text style={{fontSize: RFPercentage(4), fontWeight: 'bold'}}>{title}</Text>
 );
 
-const LeftTitle = () => {
-  const navigation = useNavigation();
-  return (
-    <Icon
-      name="arrow-back"
-      size={iconSize}
-      style={{marginLeft: marginSize}}
-      onPress={() => navigation.navigate('Home')}
-    />
-  );
-};
+// const LeftTitle = () => {
+//   const navigation = useNavigation();
+//   return (
+//     <Icon
+//       name="arrow-back"
+//       size={iconSize}
+//       style={{marginLeft: marginSize}}
+//       onPress={() => navigation.navigate('Home')}
+//     />
+//   );
+// };
 
 function BottomTabNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'pink',
+          tabBarInactiveTintColor: 'gray',
+        }}
+        initialRouteName="Home">
         <Tab.Screen
           name="칭호"
           component={TitleScreen}
           options={() => ({
             headerTitle: () => <HeaderTitle title="칭호" />,
             headerTitleAlign: 'center',
-            headerLeft: () => <LeftTitle />,
             tabBarIcon: ({color, size}) => (
               <Icon name="emoji-events" color={color} size={size} />
             ),
@@ -59,11 +62,20 @@ function BottomTabNavigator() {
           options={{
             headerTitle: () => <HeaderTitle title="캘린더" />,
             headerTitleAlign: 'center',
-            headerLeft: () => <LeftTitle />,
             tabBarIcon: ({color, size}) => (
               <Icon name="calendar-month" color={color} size={size} />
             ),
           }}
+        />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={() => ({
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <Icon name="house" color={color} size={iconSize} />
+            ),
+          })}
         />
         <Tab.Screen
           name="내 정보"
@@ -71,7 +83,6 @@ function BottomTabNavigator() {
           options={{
             headerTitle: () => <HeaderTitle title="내 정보" />,
             headerTitleAlign: 'center',
-            headerLeft: () => <LeftTitle />,
             tabBarIcon: ({color, size}) => (
               <Icon name="person" color={color} size={size} />
             ),
@@ -83,7 +94,6 @@ function BottomTabNavigator() {
           options={{
             headerTitle: () => <HeaderTitle title="설정" />,
             headerTitleAlign: 'center',
-            headerLeft: () => <LeftTitle />,
             tabBarIcon: ({color, size}) => (
               <Icon name="settings" color={color} size={size} />
             ),
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,82 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Animated, Dimensions, Text} from 'react-native';
+import View = Animated.View;
+
+const windowWidth = Dimensions.get('window').width;
+const iconSize = windowWidth * 0.095;
+const marginSize = windowWidth * 0.15;
+const smoallMarginSize = windowWidth * 0.02;
+const baseSize = windowWidth * 0.05;
+const smallSize = windowWidth * 0.02;
+const bigSize = windowWidth * 0.07;
 
 function HomeScreen() {
-  return <Text>Home</Text>;
+  return (
+    <View style={{flex: 1}}>
+      <TodaySteps />
+      <Character />
+      <TitleAndNickname />
+    </View>
+  );
+}
+
+function TodaySteps() {
+  const [todaySteps, setTodaySteps] = React.useState(12345);
+  return (
+    <View>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: marginSize,
+        }}>
+        <Text style={{fontSize: baseSize}}>오늘의 걸음 수</Text>
+      </View>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: smallSize,
+        }}>
+        <Text style={{fontSize: bigSize}}>
+          <Text style={{color: 'pink'}}>{todaySteps}</Text> 걸음
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+function Character() {
+  return (
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+      </View>
+    </View>
+  );
+}
+
+function TitleAndNickname() {
+  const [adjectiveTitle, setAdjectiveTitle] = React.useState('마음먹은');
+  const [noneTitle, setNoneTitle] = React.useState('산책러');
+  const [nickname, setNickname] = React.useState('홍길동');
+  return (
+    <View style={{marginBottom: marginSize}}>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{fontSize: baseSize}}>
+          <Text>{adjectiveTitle}</Text>
+          <Text> {noneTitle}</Text>
+        </Text>
+      </View>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{fontSize: bigSize}}>{nickname}</Text>
+      </View>
+    </View>
+  );
 }
 
 export default HomeScreen;
